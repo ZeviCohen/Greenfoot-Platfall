4fe71f28940b52fcf9016debf88015486ef225cc
ZeviCohen <123010327+ZeviCohen@users.noreply.github.com>
2023-06-12 13:13:17 -0400

    Created Project

--- Platfall.java
+++ Platfall.java
@@ -6,16 +6,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class Platfall extends World
 {
 
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public Platfall()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 600, 1); 
+    }
+    
+    public void act(){
+        if (Greenfoot.getRandomNumber(100)<2){
+            addObject(new Platform(), Greenfoot.getRandomNumber(740)+30, 0);
+        }
     }
 }
