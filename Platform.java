e554f81480615ed33a9c3c074d6ed13b3f195a9a
ZeviCohen <123010327+ZeviCohen@users.noreply.github.com>
2023-06-12 13:28:21 -0400

    Platform code added

--- Platform.java
+++ Platform.java
@@ -1,4 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList; 
+import java.util.Arrays;
 
 /**
  * Write a description of class Platform here.
@@ -8,12 +10,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Platform extends Actor
 {
+    //To assign a number to each platform
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
+    public Integer number = numbers.get(Greenfoot.getRandomNumber(numbers.size()));
+    
+    //Define image
+    GreenfootImage image = getImage();
     /**
      * Act - do whatever the Platform wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+       getWorld().showText(number + "", getX()+(image.getWidth()/2),getY()+(image.getHeight()/2));
     }
 }
